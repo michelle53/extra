6fcb0e7008387280b55a51f7ad7ef0f0571e9a75
root <root@localhost.localdomain>
2019-05-13 18:43:05 -0400

    Minor change

--- start2.py
+++ start2.py
@@ -1,13 +1,7 @@
-from TCP_Receiver2 import TCP_Receiver
+from TCP_Receiver import TCP_Receiver
+
 from TCP_Sender import TCP_Sender
 import random
-
-snd = TCP_Sender()
-rc = TCP_Receiver()
-
-#rc.start()
-# for i in range(20):
-#     snd.start("127.0.0.1")
 
 from scapy.all import *
 
@@ -15,9 +9,16 @@ sport = random.randint(1024,65535)
 
 # SYN
 ip=IP(src='127.0.0.1',dst='127.0.0.1')
-SYN=TCP(sport=sport,dport=443,flags='S',seq=1000)
+SYN=TCP(sport=80,dport=80,flags='S',seq=100)
 SYNACK=sr1(ip/SYN)
 
+print(SYNACK)
 # SYN-ACK
-ACK=TCP(sport=sport, dport=443, flags='A', seq=SYNACK.ack + 1, ack=SYNACK.seq + 1)
+my_ack = SYNACK.seq + 1
+ACK=TCP(sport=80, dport=80, flags='A', seq=101 , ack=my_ack)
 send(ip/ACK)
+
+payload = "something"
+TCP_PUSH = TCP(sport=80, dport=80, flags="PA", seq=102, ack=my_ack)
+send(ip/TCP_PUSH/payload)
+reply = sr2(ip/TCP_PUSH/payload, timeout=10)
